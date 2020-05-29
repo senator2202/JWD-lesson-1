8b9c67bfa51b1907b80b78449f2088eb339e8b38
Alexey <senator220291@gmail.com>
2020-05-29 13:59:55 +0300

    JWD - Lesson 1, task 6,7,9,10

---
+++ src/Epam/Alexey/Kharitonov/Task10/Task10.java
@@ -0,0 +1,45 @@
+package Epam.Alexey.Kharitonov.Task10;
+
+import java.util.Scanner;
+
+/*Класс хранит начало отрезка, его конец, шаг функции,
+* а также имеет метод для вывода значений функции на отрезке с шагом*/
+public class Task10 {
+    private double a;
+    private double b;
+    private double h;
+
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+    }
+
+    /*Метод выводит значения функции на отрезке с шагом*/
+    public void printFunction() {
+        System.out.println("for F(x)=tan(x):\n");
+        for (double i=a; i<=b; i+=h) {
+            System.out.println("x="+i+"; F(x)="+Math.tan(i));
+        }
+    }
+
+    /*Точка входа в программу, демонстрация работы*/
+    public static void main(String []args) {
+        Scanner scanner=new Scanner(System.in);
+        Task10 task=new Task10();
+        System.out.print("Enter start of segment: a=");
+        task.setA(scanner.nextDouble());
+        System.out.print("\nEnter end of segment: b=");
+        task.setB(scanner.nextDouble());
+        System.out.print("\nEnter step: h=");
+        task.setH(scanner.nextDouble());
+        task.printFunction();
+    }
+}
---
+++ src/Epam/Alexey/Kharitonov/Task7/Task7.java
@@ -0,0 +1,55 @@
+package Epam.Alexey.Kharitonov.Task7;
+
+import java.util.Scanner;
+
+/*Класс хранит координаты 2 точек и предоставляет метод для определения,
+* которая из точек находится ближе к началу координат*/
+public class Task7 {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+
+    /*Метод возвращает имя точки с наименьшим расстоянием до начала координат*/
+    public String closestPoint() {
+        double distance1=Math.sqrt((x1*x1)+(y1*y1));
+        double distance2=Math.sqrt((x2*x2)+(y2*y2));
+        return (distance1<distance2) ? "A" : "B";
+    }
+
+    /*Точка входа в программу, демонстрация работы*/
+    public static void main(String []args) {
+        Scanner scanner=new Scanner(System.in);
+        Task7 task=new Task7();
+        System.out.print("Enter coordinates of point A: \n"
+                +"x1=");
+        task.setX1(scanner.nextInt());
+        System.out.print("\ny1=");
+        task.setY1(scanner.nextInt());
+        System.out.print("\nEnter coordinates of point B: \n"
+                +"x2=");
+        task.setX2(scanner.nextInt());
+        System.out.print("\ny2=");
+        task.setY2(scanner.nextInt());
+
+        System.out.println("\nPoint "+task.closestPoint()
+                +" closer to origin! ");
+    }
+
+}
