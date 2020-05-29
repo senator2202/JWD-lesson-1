2186e5e2c79174c2a18f2a92c73f2b83922429cd
Alexey <senator220291@gmail.com>
2020-05-29 12:35:37 +0300

    Epam - Lesson 1

---
+++ src/Epam/Alexey/Kharitonov/Task1/Task1.java
@@ -0,0 +1,41 @@
+package Epam.Alexey.Kharitonov.Task1;
+
+import java.util.Scanner;
+
+/*Класс хранит число, его последнюю цифру,
+* и предоставляет метод для нахождения последней цифры квадарата этого числа*/
+public class Task1 {
+
+    private int number;//само число
+    private int lastDigit;//последняя цифра в этом числе
+
+    /*Конструктор, в который сразу передается число*/
+    public Task1(int number) {
+        this.number=number;
+        lastDigit=defineLastDigit(number);
+    }
+
+    /*Метод возвращает последнюю цифру апередаваемого числа*/
+    private int defineLastDigit(int number) {
+        String string=Integer.toString(number);
+        return string.toCharArray()[string.length()-1]-48;
+    }
+
+    /*Метод вовзращает последнюю цифру квадрата числа,
+    * которое хранитсяч в объекте*/
+    public int getLastDigitOfSquare() {
+        return defineLastDigit(lastDigit*lastDigit);
+    }
+
+    /*Точка входа, демонстрация работы с классом*/
+    public static void main (String []args){
+        int number;
+        Scanner scanner=new Scanner(System.in);
+        Task1 task;
+        System.out.println("Enter number: ");
+        number=scanner.nextInt();
+        task=new Task1(number);
+        System.out.println("Last digit of squared number: "
+                +task.getLastDigitOfSquare());
+    }
+}
---
+++ src/Epam/Alexey/Kharitonov/Task3/Task3.java
@@ -0,0 +1,47 @@
+package Epam.Alexey.Kharitonov.Task3;
+
+import java.util.Scanner;
+
+/*Класс хранит площадь описанного квадрата и его сторону,
+* предоставляет методы вычисления площади вписанного квадрата
+* и соотношение 2 площадей*/
+public class Task3 {
+    /*сторона описанного квадрата, он же диаметр вписанной окружности*/
+    private double side;
+    private double area;//площадь заданного квадрата
+
+    public Task3(double area) {
+        side=Math.sqrt(area);
+        this.area=area;
+    }
+
+    /**Метод вычисляет площадь вписанного
+     * в окружность квадарат по формуле D^2/2*/
+    public double inscribedSquareArea() {
+        return side*side/2;
+    }
+
+    /*Метод вычисляет, во сколько раз площадь вписанного
+    * квадрата меньше площади заданного*/
+    public double areaScale() {
+        return area/inscribedSquareArea();
+    }
+
+    public static void main(String []args) {
+        Scanner scanner=new Scanner(System.in);
+        Task3 task;
+        double area;
+        System.out.println("Enter square area: ");
+        area = scanner.nextDouble();
+        while (area<0) {
+            System.out.println("Wrong value!!! Enter it again!!!");
+            area= scanner.nextDouble();
+        }
+
+        task=new Task3(area);
+        System.out.println("Area of inscribed square: "
+                +task.inscribedSquareArea());
+        System.out.println("Described/inscribed area scale: "
+                +task.areaScale());
+    }
+}
